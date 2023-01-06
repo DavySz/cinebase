f6733c3b3753ee37bd61d041a2fb1aac3e0c238c
davy <davydesouzabar@gmail.com>
2023-01-06 15:31:33 -0400

    feat: extends texts props

--- src/components/Text/text.tsx
+++ src/components/Text/text.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Container } from "./text.styles";
 import { IText } from "./text.types";
 
-export function Text({ children, size, color, font, mb }: IText) {
+export function Text({ children, size, color, font, mb, ...rest }: IText) {
   return (
-    <Container size={size} color={color} font={font} mb={mb}>
+    <Container size={size} color={color} font={font} mb={mb} {...rest}>
       {children}
     </Container>
   );
--- src/components/Text/text.types.ts
+++ src/components/Text/text.types.ts
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
+import { TextProps } from "react-native";
 import { TColors, TFonts } from "../../global/theme/theme";
 
-export interface IText {
+export interface IText extends TextProps {
   mb?: number;
   children: ReactNode;
   font: TFonts;
   color: TColors;
-  size: "8" | "16" | "20" | "24" | "48";
+  size: "8" | "12" | "16" | "20" | "24" | "48";
 }
