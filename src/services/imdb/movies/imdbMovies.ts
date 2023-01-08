6ed791f0e02510662030004e888055dc12bdefc2
davy <davydesouzabar@gmail.com>
2023-01-07 20:23:20 -0400

    feat: create get movie method

--- src/services/imdb/movies/imdbMovies.ts
+++ src/services/imdb/movies/imdbMovies.ts
@@ -1,15 +1,23 @@
 import { IMDB_API_KEY } from "@env";
 import { imdb_movies_api } from "../../../config/axios/imdb/movies";
 import {
-  IGetPopularMovies,
+  TGetMovieResponse,
   TGetPopularMoviesResponse,
 } from "./imdbMovies.types";
 
-export async function getPopularMovies({
-  page,
-}: IGetPopularMovies): Promise<TGetPopularMoviesResponse> {
+export async function getPopularMovies(
+  page: number
+): Promise<TGetPopularMoviesResponse> {
   const { data } = await imdb_movies_api.get(
     `/3/movie/popular?api_key=${IMDB_API_KEY}&language=pt-BR&page=${page}`
+  );
+
+  return data;
+}
+
+export async function getMovie(id: number): Promise<TGetMovieResponse> {
+  const { data } = await imdb_movies_api.get(
+    `/3/movie/${id}?api_key=${IMDB_API_KEY}&language=pt-BR`
   );
 
   return data;
