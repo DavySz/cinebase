92587963f0a2e12c534277ce4e869b8befc735cc
davy <davydesouzabar@gmail.com>
2023-01-07 20:21:32 -0400

    feat: add navigation to details screen

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -1,17 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { FlatList, Keyboard, TouchableWithoutFeedback } from "react-native";
-import { Button } from "../../components/Button/button";
 import { Input } from "../../components/Input/input";
 import { MovieCard } from "../../components/MovieCard/movieCard";
+import { EmptyState } from "../../components/ScreenTemplate/EmptyState/emptyState";
 import { ScreenTemplate } from "../../components/ScreenTemplate/screenTemplate";
 import { TScreenState } from "../../components/ScreenTemplate/screenTemplate.types";
+import { Text } from "../../components/Text/text";
 import { getPopularMovies } from "../../services/imdb/movies/imdbMovies";
 import { TGetPopularMoviesResponse } from "../../services/imdb/movies/imdbMovies.types";
 import { FooterList } from "./FooterList/footerList";
 import { TFooterListState } from "./FooterList/footerList.types";
-import { Container, Separator } from "./home.styles";
+import { CardWrapper, Container, Separator } from "./home.styles";
 
 export function Home() {
+  const { navigate } = useNavigation();
   const [popularMovies, setPopularMovies] = useState<TGetPopularMoviesResponse>(
     {} as TGetPopularMoviesResponse
   );
@@ -24,10 +27,15 @@ export function Home() {
   async function loadData() {
     try {
       setScreenState("loading");
-      const response = await getPopularMovies({ page });
+      const response = await getPopularMovies(page);
       setPopularMovies(response);
       setPage((previous) => previous + 1);
-      setScreenState("ready");
+
+      if (response.results.length > 0) {
+        setScreenState("ready");
+      } else {
+        setScreenState("empty");
+      }
     } catch {
       setScreenState("error");
     }
@@ -41,7 +49,7 @@ export function Home() {
       return;
     }
     try {
-      const response = await getPopularMovies({ page });
+      const response = await getPopularMovies(page);
       setPopularMovies((previous) => ({
         ...previous,
         results: [...previous.results, ...response.results],
@@ -64,27 +72,46 @@ export function Home() {
       >
         <Container>
           <Input placeholder="Buscar filme..." mb={16} />
-          <FlatList
-            data={popularMovies.results}
-            keyExtractor={(item) => String(item.id)}
-            renderItem={({ item }) => (
-              <MovieCard
-                imagePath={item.poster_path}
-                title={item.original_title}
-                overview={item.overview}
-              />
-            )}
-            ItemSeparatorComponent={() => <Separator />}
-            showsVerticalScrollIndicator={false}
-            onEndReached={() => loadMoreData()}
-            onEndReachedThreshold={0.5}
-            ListFooterComponent={
-              <FooterList
-                footerState={footerListState}
-                errorRecoveryCallback={() => loadMoreData()}
-              />
-            }
-          />
+          {screenState === "ready" && (
+            <FlatList
+              data={popularMovies.results}
+              keyExtractor={(item) => String(item.id)}
+              renderItem={({ item, index }) => (
+                <>
+                  {index === 0 && (
+                    <Text
+                      color="background_primary"
+                      font="secondary_600"
+                      size="20"
+                      mb={24}
+                    >
+                      Populares do momento:
+                    </Text>
+                  )}
+                  <CardWrapper
+                    onPress={() => navigate("MovieDetails", { id: item.id })}
+                  >
+                    <MovieCard
+                      imagePath={item.poster_path}
+                      title={item.title}
+                      overview={item.overview}
+                    />
+                  </CardWrapper>
+                </>
+              )}
+              ItemSeparatorComponent={() => <Separator />}
+              showsVerticalScrollIndicator={false}
+              onEndReached={() => loadMoreData()}
+              onEndReachedThreshold={0.5}
+              ListFooterComponent={
+                <FooterList
+                  footerState={footerListState}
+                  errorRecoveryCallback={() => loadMoreData()}
+                />
+              }
+            />
+          )}
+          {screenState === "empty" && <EmptyState />}
         </Container>
       </ScreenTemplate>
     </TouchableWithoutFeedback>
