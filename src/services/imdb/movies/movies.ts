0b653d4ce69614d9381b80dc66416942c809e9b7
davy <davydesouzabar@gmail.com>
2023-01-08 20:18:39 -0400

    feat: create search feature

---
+++ src/services/imdb/movies/movies.ts
@@ -0,0 +1,21 @@
+import { IMDB_API_KEY } from "@env";
+import { imdb_movies_api } from "../../../config/axios/imdb/movies";
+import { TGetMovieResponse, TGetPopularMoviesResponse } from "./movies.types";
+
+export async function getPopularMovies(
+  page: number
+): Promise<TGetPopularMoviesResponse> {
+  const { data } = await imdb_movies_api.get(
+    `/3/movie/popular?api_key=${IMDB_API_KEY}&language=pt-BR&page=${page}`
+  );
+
+  return data;
+}
+
+export async function getMovie(id: number): Promise<TGetMovieResponse> {
+  const { data } = await imdb_movies_api.get(
+    `/3/movie/${id}?api_key=${IMDB_API_KEY}&language=pt-BR`
+  );
+
+  return data;
+}
