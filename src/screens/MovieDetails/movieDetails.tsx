47bd434021368a9fe039c5f08efb4fcf41e3ca6e
davy <davydesouzabar@gmail.com>
2023-01-08 20:18:23 -0400

    feat: use screen states

--- src/screens/MovieDetails/movieDetails.tsx
+++ src/screens/MovieDetails/movieDetails.tsx
@@ -2,9 +2,8 @@ import { IMDB_IMAGES_BASE_URL } from "@env";
 import { useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { ScreenTemplate } from "../../components/ScreenTemplate/screenTemplate";
-import { TScreenState } from "../../components/ScreenTemplate/screenTemplate.types";
-import { getMovie } from "../../services/imdb/movies/imdbMovies";
-import { TGetMovieResponse } from "../../services/imdb/movies/imdbMovies.types";
+import { getMovie } from "../../services/imdb/movies/movies";
+import { TGetMovieResponse } from "../../services/imdb/movies/movies.types";
 import {
   Container,
   Content,
@@ -22,6 +21,8 @@ import { View } from "react-native";
 import { formatToBRL } from "brazilian-values";
 import { InfoCard } from "./InfoCard/infoCard";
 import { Chip } from "../../components/Chip/Chip";
+import { ScreenState } from "../../components/ScreenState/screenState";
+import { TScreenState } from "../../components/ScreenState/ScreenState.types";
 
 export function MovieDetails() {
   const { colors } = useTheme();
@@ -87,73 +88,73 @@ export function MovieDetails() {
   }, []);
 
   return (
-    <ScreenTemplate
-      errorRecoveryCallback={() => {}}
-      screenState={screenState}
-      previousRoute="Home"
-      title="Detalhes"
-    >
-      <Container>
-        <ImageWrapper
-          source={{ uri: `${IMDB_IMAGES_BASE_URL}${movie.backdrop_path}` }}
-        >
-          <Gradient>
-            <View>{renderStars()}</View>
-            <Text color="background_primary" size="20" font="secondary_600">
-              {movie.release_date}
+    <ScreenTemplate previousRoute="Home" title="Detalhes">
+      <ScreenState
+        errorRecoveryCallback={() => loadData()}
+        screenState={screenState}
+      >
+        <Container>
+          <ImageWrapper
+            source={{ uri: `${IMDB_IMAGES_BASE_URL}${movie.backdrop_path}` }}
+          >
+            <Gradient>
+              <View>{renderStars()}</View>
+              <Text color="background_primary" size="20" font="secondary_600">
+                {movie.release_date}
+              </Text>
+            </Gradient>
+          </ImageWrapper>
+          <Content>
+            <Text
+              color="background_primary"
+              size="20"
+              font="secondary_600"
+              mb={8}
+            >
+              {movie.title}
             </Text>
-          </Gradient>
-        </ImageWrapper>
-        <Content>
-          <Text
-            color="background_primary"
-            size="20"
-            font="secondary_600"
-            mb={8}
-          >
-            {movie.title}
-          </Text>
-          <MovieOverview
-            color="background_primary"
-            size="16"
-            font="primary_500"
-            mb={32}
-          >
-            {movie.overview}
-          </MovieOverview>
-          {movie.genres && (
-            <Row mb={24}>
-              {movie.genres.map((item) => {
-                return <Chip text={item.name} spacing={4} />;
-              })}
-            </Row>
-          )}
-          <InfoCard
-            icon={
-              <MaterialIcons
-                name="local-movies"
-                size={32}
-                color={colors.main}
-              />
-            }
-            info={formatRuntime(movie.runtime)}
-          />
-          <InfoCard
-            icon={
-              <MaterialIcons
-                name="attach-money"
-                size={32}
-                color={colors.main}
-              />
-            }
-            info={formatToBRL(movie.budget)}
-          />
-          <InfoCard
-            icon={<Entypo name="ticket" size={32} color={colors.main} />}
-            info={formatToBRL(movie.revenue)}
-          />
-        </Content>
-      </Container>
+            <MovieOverview
+              color="background_primary"
+              size="16"
+              font="primary_500"
+              mb={32}
+            >
+              {movie.overview}
+            </MovieOverview>
+            {movie.genres && (
+              <Row mb={24}>
+                {movie.genres.map((item) => {
+                  return <Chip text={item.name} spacing={4} />;
+                })}
+              </Row>
+            )}
+            <InfoCard
+              icon={
+                <MaterialIcons
+                  name="local-movies"
+                  size={32}
+                  color={colors.main}
+                />
+              }
+              info={formatRuntime(movie.runtime)}
+            />
+            <InfoCard
+              icon={
+                <MaterialIcons
+                  name="attach-money"
+                  size={32}
+                  color={colors.main}
+                />
+              }
+              info={formatToBRL(movie.budget)}
+            />
+            <InfoCard
+              icon={<Entypo name="ticket" size={32} color={colors.main} />}
+              info={formatToBRL(movie.revenue)}
+            />
+          </Content>
+        </Container>
+      </ScreenState>
     </ScreenTemplate>
   );
 }
