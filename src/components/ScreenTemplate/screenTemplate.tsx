ba0108c57d011420c976bbe8f40dccf621f71856
davy <davydesouzabar@gmail.com>
2023-01-08 20:17:27 -0400

    feat: create screen states

--- src/components/ScreenTemplate/screenTemplate.tsx
+++ src/components/ScreenTemplate/screenTemplate.tsx
@@ -1,26 +1,17 @@
 import React from "react";
 import { Header } from "../Header/header";
-import { Loading } from "../Loading/loading";
-import { ErrorState } from "./ErrorState/errorState";
 import { Container } from "./screenTemplate.styles";
 import { IScreenTemplate } from "./screenTemplate.types";
 
 export function ScreenTemplate({
   title,
   children,
-  screenState,
   previousRoute,
-  errorRecoveryCallback,
 }: IScreenTemplate) {
   return (
     <Container>
       <Header previousRoute={previousRoute} title={title} />
-      {screenState === "ready" && children}
-      {screenState === "empty" && children}
-      {screenState === "loading" && <Loading full />}
-      {screenState === "error" && (
-        <ErrorState errorRecoveryCallback={() => errorRecoveryCallback()} />
-      )}
+      {children}
     </Container>
   );
 }
--- src/components/ScreenTemplate/screenTemplate.types.ts
+++ src/components/ScreenTemplate/screenTemplate.types.ts
@@ -1,10 +1,6 @@
 import { ReactNode } from "react";
 import { IHeader } from "../Header/header.types";
 
-export type TScreenState = "ready" | "empty" | "error" | "loading";
-
 export interface IScreenTemplate extends IHeader {
   children: ReactNode;
-  screenState: TScreenState;
-  errorRecoveryCallback: () => void;
 }
