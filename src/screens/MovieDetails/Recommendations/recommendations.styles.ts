31b7f0fffa05b3aa420b028b0b257527de397f6d
davy <davydesouzabar@gmail.com>
2023-01-09 21:11:03 -0400

    feat: create recommendations list

---
+++ src/screens/MovieDetails/Recommendations/recommendations.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const CardWrapper = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})``;
+
+export const Separator = styled.View`
+  height: 24px;
+`;
