5d68e5ed0f7940a5a21138bef9b7dde26e3c8580
davy <davydesouzabar@gmail.com>
2023-01-13 15:22:49 -0400

    feat: create signin screen

---
+++ src/screens/SignIn/signIn.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+
+  background-color: ${({ theme }) => theme.colors.shape_dark};
+
+  padding: 62px 24px 24px 24px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const GhostView = styled.View`
+  width: 32px;
+`;
