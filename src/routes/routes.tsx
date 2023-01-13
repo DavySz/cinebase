4e9794fc2280b0fe158b2cc175b201238ec4b66b
davy <davydesouzabar@gmail.com>
2023-01-13 15:24:21 -0400

    feat: create tab routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useAuth } from "../hooks/useAuth/useAuth";
+import { LoginRoutes } from "./login.routes";
 import { StackRoutes } from "./stack.routes";
+import { TabRoutes } from "./tab.routes";
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {user.id ? <StackRoutes /> : <LoginRoutes />}
     </NavigationContainer>
   );
 }
