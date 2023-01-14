c45e54a13e30e4993d9e6a2c31bb0b9ef816ca4f
davy <davydesouzabar@gmail.com>
2023-01-14 16:34:40 -0400

    feat: pass card optional prop

--- src/components/MovieCard/movieCard.tsx
+++ src/components/MovieCard/movieCard.tsx
@@ -1,33 +1,45 @@
 import { IMDB_IMAGES_BASE_URL } from "@env";
 import React from "react";
-import { Container, ImageWrapper, Header, Footer } from "./movieCard.styles";
+import {
+  Container,
+  ImageWrapper,
+  Footer,
+  TextWrapper,
+  IconWrapper,
+  PressWrapper,
+} from "./movieCard.styles";
 import { IMovieCard } from "./movieCard.types";
 import { Text } from "../Text/text";
 
-function MovieCard({ imagePath, title, overview }: IMovieCard) {
+function MovieCard({ imagePath, title, overview, icon, onPress }: IMovieCard) {
   return (
     <Container>
-      <ImageWrapper
-        source={{ uri: `${IMDB_IMAGES_BASE_URL}${imagePath}` }}
-        borderRadius={8}
-      />
+      <PressWrapper onPress={onPress}>
+        <ImageWrapper
+          source={{ uri: `${IMDB_IMAGES_BASE_URL}${imagePath}` }}
+          borderRadius={8}
+        />
+      </PressWrapper>
       <Footer>
-        <Text
-          color="background_primary"
-          font="secondary_600"
-          numberOfLines={1}
-          size="16"
-        >
-          {title}
-        </Text>
-        <Text
-          color="background_primary"
-          font="primary_400"
-          numberOfLines={1}
-          size="12"
-        >
-          {overview}
-        </Text>
+        <TextWrapper>
+          <Text
+            color="background_primary"
+            font="secondary_600"
+            numberOfLines={1}
+            size="16"
+          >
+            {title}
+          </Text>
+          <Text
+            color="background_primary"
+            font="primary_400"
+            numberOfLines={1}
+            size="12"
+          >
+            {overview}
+          </Text>
+        </TextWrapper>
+        <IconWrapper>{icon && icon}</IconWrapper>
       </Footer>
     </Container>
   );
--- src/components/MovieCard/movieCard.types.ts
+++ src/components/MovieCard/movieCard.types.ts
@@ -1,5 +1,9 @@
+import { ReactNode } from "react";
+
 export interface IMovieCard {
   imagePath: string;
   title: string;
   overview: string;
+  icon?: ReactNode;
+  onPress: () => void;
 }
