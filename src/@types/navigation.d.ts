6b3fc717925d32266b507a3e79e69f548dc88c38
davy <davydesouzabar@gmail.com>
2023-01-16 09:30:52 -0400

    feat: update navigation

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,12 +2,18 @@ export declare global {
   namespace ReactNavigation {
     export interface RootParamList {
       SignIn: undefined;
+      MovieReviews: {
+        id: number;
+      };
       Home: undefined;
       MovieDetails: {
         id: number;
       };
       Actors: {
         id: number;
+      };
+      ReviewDetails: {
+        id: string;
       };
     }
   }
